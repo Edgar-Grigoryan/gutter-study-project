887d20572370915d43085aad95bc3ecdecb5aae0
Edgar Grigoryan <edgar.grig.2001@gmail.com>
2022-02-24 23:09:07 +0400

    Added havayi tox.

--- MyCanvas/ViewController.swift
+++ MyCanvas/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.addSubview(zoomView)
-        
+        // aaaaaaaa
     }
 
    var zoomView = ZoomView(frame: .init(x: 0, y: 0, width: 3000, height: 2000))
